973e2eff41775fba766c0f7df8c2c15d4ad05aa5
Ilmar Khabibullin <ilmarhabibullin@yandex.ru>
2022-08-21 12:54:16 +0300

    Initital commit

---
+++ Start.java
@@ -0,0 +1,15 @@
+public class Start {
+
+    public static void main(String[] args) {
+
+     
+        // amount- стоимость билета.
+        // mille- мили.
+
+        int amount = 9215;
+        int mille= 20;
+
+        System.out.println("Ваши мили: " + amount / mille);
+
+    }
+}
