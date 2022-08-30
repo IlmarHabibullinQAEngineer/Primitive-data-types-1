c692505f76d9ad778492c760d1b8f65723f8d963
Ilmar Khabibullin <ilmarhabibullin@yandex.ru>
2022-08-30 08:44:29 +0300

    Renamed to Main

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
-public class Start {
+public class Main {
 
     public static void main(String[] args) {
 
-     
+
         // amount- стоимость билета.
         // mille- мили.
 
         int amount = 9215;
-        int mille= 20;
+        int mille = 20;
 
         System.out.println("Ваши мили: " + amount / mille);
 
